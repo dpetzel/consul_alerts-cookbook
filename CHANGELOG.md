59d1e99e0197ebee18597ace9b66f48abd9ddca2
dpetzel <davidpetzel@gmail.com>
2014-11-09 13:32:01 -0500

    version bump

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,4 @@
 # Consul-Alerts Cookbook Change Log
 
-
-## v1.0.0 (UNRELEASED)
+## v1.0.0 (Nov 9, 2014)
 * Initial Release
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,12 @@ maintainer_email 'davidpetzel@gmail.com'
 license 'apache2'
 description 'Installs/Configures consul-alerts'
 long_description 'Installs/Configures consul-alerts'
-version '0.9.9'
+version '1.0.0'
 
 depends 'service_factory', '~> 0.1.4'
 depends 'consul_kv', '~> 1.0.0'
+
+%w(redhat centos).each do |name|
+  supports name, '~> 6.0'
+  supports name, '~> 5.0'
+end
